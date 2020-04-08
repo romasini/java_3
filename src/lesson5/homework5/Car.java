72d426a1abd0552b9c0d8cbfc6a6aaaaf0bb9ecf
Roman Sinitsyn <romasini@gmail.com>
2020-04-08 13:02:09 +0300

    домашнее задание 5

--- src/lesson5/homework5/Car.java
+++ src/lesson5/homework5/Car.java
@@ -1,20 +1,26 @@
 package lesson5.homework5;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Car implements Runnable{
-    private static int CARS_COUNT;//количество машин
+    private static int CARS_COUNT = 0;//количество машин
     private Race race; //трасса
     private int speed; //скорость
     private String name;
+    private CountDownLatch prepareCDL;
+    private CountDownLatch finishCDL;
 
     public String getName(){
         return name;
     }
 
-    public Car(Race race, int speed){
+    public Car(Race race, int speed, CountDownLatch prepareCDL, CountDownLatch finishCDL){
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник № "+ CARS_COUNT;
+        this.finishCDL = finishCDL;
+        this.prepareCDL = prepareCDL;
     }
 
     @Override
@@ -23,12 +29,21 @@ public class Car implements Runnable{
             System.out.println(this.name + " готовится");
             Thread.sleep(500+(int)(Math.random()*800));
             System.out.println(this.name + " готов");
+            prepareCDL.countDown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            prepareCDL.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         for (int i = 0; i< race.getStages().size(); i++){
             race.getStages().get(i).go(this);
         }
+
+        finishCDL.countDown();
+
     }
 
     public int getSpeed() {
