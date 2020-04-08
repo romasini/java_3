c2950ad0e650bf37538a4e925fcdf20ede516d6b
Roman Sinitsyn <romasini@gmail.com>
2020-04-08 09:22:36 +0300

    подготовка ДЗ 5

---
+++ src/lesson5/homework5/Stage.java
@@ -0,0 +1,10 @@
+package lesson5.homework5;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public String getDescription(){
+        return description;
+    }
+    public abstract void go(Car C);
+}
